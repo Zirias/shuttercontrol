8f2cab5e253dc69088149c9cc737f2525149c51a
Felix Palmen <felix@palmen-it.de>
2015-08-14 04:57:37 +0200

    initialize port pullups as early as possible

--- shutterctl_attiny84/main.c
+++ shutterctl_attiny84/main.c
@@ -13,14 +13,22 @@ AVR_MCU(1000000, "attiny84");
 AVR_MCU_VOLTAGES(5000, 5000, 5000);
 AVR_MCU_SIMAVR_CONSOLE(&GPIOR0);
 
-void __attribute__((__noreturn__)) main(void)
+void __attribute__((__naked__)) __init(void)
 {
     /* initial pin configuration */
-    DDRA = 0xc0;
-    PORTA = 0x3f;
-    DDRB = 0x00;
-    PORTB = 0x0f;
+    __asm__ volatile (
+	    "ldi    r16, 0x3f	\n\t"
+	    "out    %0, r16	\n\t"
+	    "out    %1, r16	\n\t"
+	    "ldi    r16, 0xc0	\n\t"
+	    "out    %2, r16	\n"
+	    :
+	    : "I" _SFR_IO_ADDR(PORTA), "I" _SFR_IO_ADDR(PORTB),
+	      "I" _SFR_IO_ADDR(DDRA));
+}
 
+void __attribute__((__naked__,__noreturn__)) main(void)
+{
     /* initialize all modules */
     eepdata_init();
     event_init();
